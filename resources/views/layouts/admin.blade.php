48c866e7718d1935b81e4d969a1027b05b87cf94
daiiseventeen <dudidenz17@gmail.com>
2026-01-15 10:56:48 +0700

    add front end welcome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,17 +7,14 @@
 <body class="flex bg-gray-100">
 
     {{-- Sidebar --}}
-    <aside class="w-64 bg-black text-white min-h-screen p-4 flex flex-col">
+    <aside class="w-64 bg-black text-white min-h-screen p-4">
         <h2 class="text-xl font-bold mb-6">Admin CINEGO</h2>
-        <ul class="flex-1">
+        <ul>
             <li class="mb-3"><a href="#">Dashboard</a></li>
             <li class="mb-3"><a href="#">Film</a></li>
             <li class="mb-3"><a href="#">Jadwal</a></li>
             <li class="mb-3"><a href="#">Transaksi</a></li>
         </ul>
-        <button onclick="document.getElementById('logout-form').submit()" class="w-full bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            Logout
-        </button>
     </aside>
 
     <form id="logout-form" method="POST" action="{{ route('logout') }}">
