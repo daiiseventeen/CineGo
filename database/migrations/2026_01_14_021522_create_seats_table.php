52b3d73fe5f5d77f063862730cc2aca5754e5715
daiiseventeen <dudidenz17@gmail.com>
2026-01-15 07:56:16 +0700

    add migration and login register

---
+++ database/migrations/2026_01_14_021522_create_seats_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('studio_id')->constrained()->cascadeOnDelete();
+            $table->string('seat_code');
+            $table->enum('type', ['regular', 'vip']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
